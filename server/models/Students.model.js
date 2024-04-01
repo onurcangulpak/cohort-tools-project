a893eff77e092be573cbbe073cb6a4a89bd2edb7
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-04-01 17:51:22 +0200

    done

--- server/models/Students.model.js
+++ server/models/Students.model.js
@@ -12,7 +12,7 @@ const studentsSchema = new Schema ({
     program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"] },
     background: {type: String, default: ""},
     image: {type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-    cohort: {type: Schema.Types.ObjectId, ref: 'Cohorts'  },
+    cohort: {type: Schema.Types.ObjectId, ref: 'Cohort'  },
     projects:  {type: [String]},
 })
 
